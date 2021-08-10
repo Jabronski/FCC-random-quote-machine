21cbd96e14404926cb256ff7f24c392149a86497
Jabronski <xierou@gmail.com>
2021-08-10 23:40:17 +0200

    completed react project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
-import RANDOM_COLOR from './myColors.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTumblr, faTwitter, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import { faTumblr, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 let quotesURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
